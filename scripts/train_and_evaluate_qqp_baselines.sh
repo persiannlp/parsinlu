38e40566a4f8a97313fd5454f37ea4bc9494c68c
Daniel Khashabi <danielk@allenai.org>
2020-10-01 14:12:49 -0700

    adding train/eval script for qqp.

---
+++ scripts/train_and_evaluate_qqp_baselines.sh
@@ -0,0 +1,15 @@
+export DATA_DIR=data/qqp
+
+python run_text_classification.py \
+  --data_dir $DATA_DIR \
+  --task_name qqp \
+  --model_name_or_path bert-base-multilingual-cased \
+  --do_train \
+  --do_eval \
+  --learning_rate 5e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 128 \
+  --output_dir /tmp/debug_qqp/ \
+  --save_steps -1
+
+
