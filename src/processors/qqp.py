5c250d02fb704751f329d7523952b05a5a5df190
Daniel Khashabi <danielk@allenai.org>
2020-10-25 21:32:23 -0700

    updating qqp data.

--- src/processors/qqp.py
+++ src/processors/qqp.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from transformers import InputExample, DataProcessor
@@ -12,47 +13,64 @@ class QQPProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         """See base class."""
-        lines = self._read_tsv(os.path.join(data_dir, "train.tsv"))
         examples = []
-        for (i, line) in enumerate(lines):
-            if i == 0:
-                continue
-            guid = "%s-%s-%s-%s" % (line[0], line[1], line[2], "train")
-            text_a = line[6]
-            text_b = line[7]
-            if len(text_a) < 5 or len(text_b) < 5:
-                continue
-            label = "paraphrase" if line[5] == "0" else "not-paraphrase"
-            assert isinstance(text_a, str), f"Training input {text_a} is not a string"
-            assert isinstance(text_b, str), f"Training input {text_b} is not a string"
-            assert isinstance(label, str), f"Training label {label} is not a string"
-            example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
-            if i < 10:
-                print(example)
-            examples.append(example)
+        with open(os.path.join(data_dir, "train.jsonl"), "r") as f:
+            for i, line in enumerate(f.readlines()):
+                json_content = json.loads(line.replace("\n", ""))
+                guid = "%s-%s" % (i, "train")
+                text_a = json_content['q1']
+                text_b = json_content['q2']
+                assert label == '1' or label == '0'
+                label = "paraphrase" if line[5] == "0" else "not-paraphrase"
+                assert isinstance(text_a, str), f"Training input {text_a} is not a string"
+                assert isinstance(text_b, str), f"Training input {text_b} is not a string"
+                assert isinstance(label, str), f"Training label {label} is not a string"
+                example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
+                if i < 10:
+                    print(example)
+                examples.append(example)
         return examples
 
     def get_test_examples(self, data_dir):
         """See base class."""
-        lines = self._read_tsv(os.path.join(data_dir, "test.tsv"))
         examples = []
-        for (i, line) in enumerate(lines):
-            if i == 0:
-                continue
-            guid = "%s-%s-%s-%s" % (line[0], line[1], line[2], "test")
-            text_a = line[6]
-            text_b = line[7]
-            if len(text_a) < 5 or len(text_b) < 5:
-                continue
-            label = "paraphrase" if line[5] == "0" else "not-paraphrase"
-            assert isinstance(text_a, str), f"Training input {text_a} is not a string"
-            assert isinstance(text_b, str), f"Training input {text_b} is not a string"
-            assert isinstance(label, str), f"Training label {label} is not a string"
-            example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
-            if i < 10:
-                print(example)
-            examples.append(example)
+        with open(os.path.join(data_dir, "test.jsonl"), "r") as f:
+            for i, line in enumerate(f.readlines()):
+                json_content = json.loads(line.replace("\n", ""))
+                guid = "%s-%s" % (i, "test")
+                text_a = json_content['q1']
+                text_b = json_content['q2']
+                assert label == '1' or label == '0'
+                label = "paraphrase" if line[5] == "0" else "not-paraphrase"
+                assert isinstance(text_a, str), f"Training input {text_a} is not a string"
+                assert isinstance(text_b, str), f"Training input {text_b} is not a string"
+                assert isinstance(label, str), f"Training label {label} is not a string"
+                example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
+                if i < 10:
+                    print(example)
+                examples.append(example)
         return examples
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        examples = []
+        with open(os.path.join(data_dir, "deb.jsonl"), "r") as f:
+            for i, line in enumerate(f.readlines()):
+                json_content = json.loads(line.replace("\n", ""))
+                guid = "%s-%s" % (i, "dev")
+                text_a = json_content['q1']
+                text_b = json_content['q2']
+                assert label == '1' or label == '0'
+                label = "paraphrase" if line[5] == "0" else "not-paraphrase"
+                assert isinstance(text_a, str), f"Training input {text_a} is not a string"
+                assert isinstance(text_b, str), f"Training input {text_b} is not a string"
+                assert isinstance(label, str), f"Training label {label} is not a string"
+                example = InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label)
+                if i < 10:
+                    print(example)
+                examples.append(example)
+        return examples
+
 
     def get_labels(self):
         """See base class."""
